a8ee284bf89119f979333641e64a5d08fad331e1
Hydren <166227780+hydren-dev@users.noreply.github.com>
2024-12-22 20:09:22 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# Log the start of the container
-echo "Container started!"
-npm run configure -- --panel "$PANEL" --key "$KEY"
-# Start the application
-echo "Starting application..."
-node index.js
+cd /home/container
